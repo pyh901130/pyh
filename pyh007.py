1766857f772ee90391be9e29b427ee245ff2a2ed
pyh901130 <123149723+pyh901130@users.noreply.github.com>
2023-01-20 16:59:01 +0900

    Update pyh007.py

--- pyh007.py
+++ pyh007.py
@@ -2,7 +2,8 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
-
+st.title('This is a title')
+st.title('A title with _italics_ :blue[colors] and emojis :sunglasses:')
 
 #plt.figure(figsize=(12,8))
 
