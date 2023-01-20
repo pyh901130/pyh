b4f54735d9c5934fcae9e3db6d147730a09ba403
pyh901130 <123149723+pyh901130@users.noreply.github.com>
2023-01-20 17:06:26 +0900

    Update pyh007.py

--- pyh007.py
+++ pyh007.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
-st.title('P.yonghun is back :blue[colors] and :sunglasses: ')
+st.title('blue[P.yonghun] : is back : and :sunglasses: ')
 
 
 #plt.figure(figsize=(12,8))
