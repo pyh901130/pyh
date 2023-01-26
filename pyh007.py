cce43c24b9e72eee3ba6afa890786317cebcb253
pyh901130 <123149723+pyh901130@users.noreply.github.com>
2023-01-26 17:20:25 +0900

    Update pyh007.py

--- pyh007.py
+++ pyh007.py
@@ -5,9 +5,9 @@ import numpy as np
 
 st.title(':blue[P.yonghun] is back :sunglasses: ')
 
-money = pd.read_csv("money_data7.csv")
+#money = pd.read_csv("money_data7.csv")
 
-st.sidebar.success("Select a demo above.")
+#st.sidebar.success("Select a demo above.")
 
 def  plotting_demo():
     money = pd.read_csv("money_data7.csv")
