6d4f5eb4a522a88faf37970017b7dbbdfb98ff0b
pyh901130 <123149723+pyh901130@users.noreply.github.com>
2023-02-01 17:12:42 +0900

    Update pyh007.py

--- pyh007.py
+++ pyh007.py
@@ -9,10 +9,10 @@ st.sidebar.success("Select a demo above.")
 
 def  plotting_demo():
     
-    uploaded_file = st.file_uploader("Choose a file")
+    #uploaded_file = st.file_uploader("Choose a file")
 
-    money=pd.read_csv(uploaded_file)
-   # money = pd.read_csv("money_data7.csv")
+    #money=pd.read_csv(uploaded_file)
+    money = pd.read_csv("money_data7.csv")
 
     option = st.selectbox(
         'How would you like to choice year ?',
